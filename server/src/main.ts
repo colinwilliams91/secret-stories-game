a2cd2ea2cc3cfd6b882db2e3fe8f3ca42237b2e7
colin <colin.williams91@gmail.com>
2023-05-28 21:32:32 -0500

    pre & post build scripts rm and mv dir to backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const options = new DocumentBuilder()
+    .setTitle('Secret-Stories')
+    .setDescription('Competitive Creative Writing Multiplayer Web Game')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
